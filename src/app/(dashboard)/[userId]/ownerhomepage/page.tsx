d7c5bd02123b6e842e682bb80ec449af254a0321
satyam-svg <satyammaurya9620@gmail.com>
2025-04-10 00:22:19 +0530

    fixing bugs

--- src/app/(dashboard)/[userId]/ownerhomepage/page.tsx
+++ src/app/(dashboard)/[userId]/ownerhomepage/page.tsx
@@ -1,7 +1,7 @@
-export default function ownerhomepage(){
-    return(
-        <>
-           <h1>hi this is homepage </h1>
-        </>
-    )
+export default function ownerhomepage() {
+  return (
+    <>
+      <h1>hi this is homepage </h1>
+    </>
+  );
 }
