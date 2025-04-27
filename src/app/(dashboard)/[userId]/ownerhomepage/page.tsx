e663b2d7736fe6466df3de7ece869949dc6656f5
satyam-svg <satyammaurya9620@gmail.com>
2025-04-28 00:31:36 +0530

    added backendd

--- src/app/(dashboard)/[userId]/ownerhomepage/page.tsx
+++ src/app/(dashboard)/[userId]/ownerhomepage/page.tsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import axios from "axios";
 
-
 const floatingStars = Array(30).fill(null);
 
 interface Salon {
