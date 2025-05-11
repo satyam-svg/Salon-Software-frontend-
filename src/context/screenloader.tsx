c84e098471bc76af78c2c1dd167655ab75a0f097
rahulcoder001 <124455574+rahulcoder001@users.noreply.github.com>
2025-05-11 17:07:29 +0530

    complited

---
+++ src/context/screenloader.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+const ScreenLoaderContext = createContext<{ ScreenLoaderToggle: boolean; setScreenLoaderToggle: (value: boolean) => void } | undefined>(undefined);
+
+export const ScreenLoaderProvider = ({ children }: { children: React.ReactNode }) => {
+  const [ScreenLoaderToggle, setScreenLoaderToggle] = useState(false);
+
+  return (
+    <ScreenLoaderContext.Provider value={{ ScreenLoaderToggle, setScreenLoaderToggle }}>
+      {children}
+    </ScreenLoaderContext.Provider>
+  );
+};
+
+export const useLogin = () => {
+  const context = useContext(ScreenLoaderContext);
+  if (!context) throw new Error("useScreenLoader must be used within a LoginProvider");
+  return context;
+};
